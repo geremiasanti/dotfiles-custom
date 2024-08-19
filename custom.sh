9215a077ad26366b01b63fd1d6693a3d68c8fb9b
geremia <germysettesette@gmail.com>
2024-08-19 12:02:03 +0200

    initial commit

---
+++ custom.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+function tmux_sessionizer() {
+	if [[ $# -eq 1 ]]; then
+		selected=$1
+	else
+		selected=$(find ~/dev -exec test -e '{}/.git' ';' -print -prune -mindepth 1 -maxdepth 4 -type d | fzf)
+	fi
+
+	if [[ -z $selected ]]; then
+		exit 0
+	fi
+
+	selected_name=$(basename "$selected" | tr . _)
+	tmux_running=$(pgrep tmux)
+
+	if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+		tmux new-session -s $selected_name -c $selected
+		exit 0
+	fi
+
+	if ! tmux has-session -t=$selected_name 2> /dev/null; then
+		tmux new-session -ds $selected_name -c $selected
+	fi
+
+	tmux switch-client -t $selected_name
+}
